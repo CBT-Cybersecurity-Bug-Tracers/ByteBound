492f04ddce5c4cbc703d95f3d4a0715752fc8609
aidan <s10243503@connect.np.edu,sg>
2023-08-04 12:18:24 +0800

    Error: when input securityqn and securityans db shows empty cell

--- Models/ApplicationUsers.cs
+++ Models/ApplicationUsers.cs
@@ -6,7 +6,7 @@ namespace ByteBound.Models
     public class ApplicationUsers : IdentityUser
     {
         public string FullName { get; set; } = string.Empty;
-        public string SecurityAns { get; set; }
-        public string SecurityQn { get; set; } 
+        public string SecurityAns { get; set; } = string.Empty;
+        public string SecurityQn { get; set; } = string.Empty;
     }
 }
